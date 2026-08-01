204eea0d719256cf3edc1936b6cf0bfa7c9fedda
kabartolo <kate.bartolo19@gmail.com>
2026-07-31 23:53:37 -0400

    fix(astro-mdx-remote): pack import errors

--- packages/astro-mdx-remote/tsup.config.js
+++ packages/astro-mdx-remote/tsup.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/renderer.ts', 'src/compiler.ts', 'src/hydrator.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
   outDir: 'dist',
+  external: ['virtual:mdx-components'],
 });
