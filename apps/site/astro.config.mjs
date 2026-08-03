c9cbc78514811b9b64da49c6e6365ccda7b997b5
kabartolo <kate.bartolo19@gmail.com>
2026-08-03 08:52:49 -0400

    chore(site): set up vercel

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -6,20 +6,22 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import rehypeComponentMarkers from './src/plugins/rehype-component-markers.js';
 import astroRemoteMdx from 'astro-mdx-remote';
-
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://katebartolo.dev',
   // output: 'server',
+  adapter: vercel(),
+
   integrations: [
     mdx(),
     sitemap(),
     icon(),
     react(),
     astroRemoteMdx({
-      configFilePath: './src/mdx-components.js'
+      configFilePath: './src/mdx-components.js',
     }),
   ],
   markdown: {
