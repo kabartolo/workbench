ec2acdd9903b839587b70cf42f3b2dad24a39a1b
kabartolo <kate.bartolo19@gmail.com>
2026-08-05 17:19:45 -0400

    fix(site): ts and lab sidebar

--- apps/site/src/pages/lab/render-markdown/sidebar.ts
+++ apps/site/src/pages/lab/render-markdown/sidebar.ts
@@ -3,20 +3,20 @@ export const sidebar = [
     label: 'Render Markdown',
     items: [
       {
-        label: 'Experiment 0',
-        path: '/lab/render-markdown/exp-0/rendering-remote-markdown-and-mdx-in-astro-5117-temp-slug-5744163',
+        label: 'The Baseline: Plain HTML Rendering',
+        path: '/lab/render-markdown/exp-0/rendering-remote-content#experiment-0-the-baseline-plain-html-rendering',
       },
       {
         label: 'Experiment 1',
-        path: '/lab/render-markdown/exp-1/rendering-remote-markdown-and-mdx-in-astro-5117-temp-slug-5744163',
+        path: '/lab/render-markdown/exp-1/rendering-remote-content#experiment-1-client-islands',
       },
       {
         label: 'Experiment 2',
-        path: '/lab/render-markdown/exp-2/rendering-remote-markdown-and-mdx-in-astro-5117-temp-slug-5744163',
+        path: '/lab/render-markdown/exp-2/rendering-remote-content#experiment-2-mdx-compiler',
       },
       {
         label: 'Experiment 3',
-        path: '/lab/render-markdown/exp-3/rendering-remote-markdown-and-mdx-in-astro-5117-temp-slug-5744163',
+        path: '/lab/render-markdown/exp-3/rendering-remote-content#experiment-3-server-render-hydration-islands',
       },
     ],
   },
